4c08e928e2fea44f4f764bff1e936ca9c3f9ea70
Nicolas Amadori - nicolas.amadori@studio.unibo.it <nicolas.amadori@studio.unibo.it>
2025-03-03 11:12:30 +0100

    test refactoring

--- src/test/java/e1/GoldBankAccountTest.java
+++ src/test/java/e1/GoldBankAccountTest.java
@@ -1,22 +1,27 @@
 package e1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GoldBankAccountTest {
 
+    private BankAccount account;
+
+    @BeforeEach
+    void init(){
+        this.account = new GoldBankAccount(new CoreBankAccount());
+    }
+
     @Test
     public void testInitiallyEmpty() {
-        BankAccount account = new GoldBankAccount(new CoreBankAccount());
-        assertEquals(0, account.getBalance());
+        assertEquals(0, this.account.getBalance());
     }
 
     @Test
     public void testCanDeposit() {
-        BankAccount account = new GoldBankAccount(new CoreBankAccount());
-
         account.deposit(1000);
-        assertEquals(1000, account.getBalance());
+        assertEquals(1000, this.account.getBalance());
     }
 }
