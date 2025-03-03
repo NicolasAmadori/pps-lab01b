5ad7d25df09a9c503765f524d48ad4a2fa1a0044
Nicolas Amadori - nicolas.amadori@studio.unibo.it <nicolas.amadori@studio.unibo.it>
2025-03-03 11:38:42 +0100

    bronz test refactoring

--- src/test/java/e1/BronzBankAccountTest.java
+++ src/test/java/e1/BronzBankAccountTest.java
@@ -4,17 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BronzBankAccountTest{
-    @Test
-    public void testInitiallyEmpty() {
-        BankAccount account = new BronzBankAccount(new CoreBankAccount());
-        assertEquals(0, account.getBalance());
-    }
+public class BronzBankAccountTest extends BankAccountTest{
 
-    @Test
-    public void testCanDeposit() {
-        BankAccount account = new BronzBankAccount(new CoreBankAccount());
-        account.deposit(1000);
-        assertEquals(1000, account.getBalance());
+    BronzBankAccountTest() {
+        super(new BronzBankAccount(new CoreBankAccount()));
     }
 }
