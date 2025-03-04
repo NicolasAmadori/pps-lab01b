d514fa0514bb34d5042910588cffe90576f105fc
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2025-03-04 13:25:43 +0100

    testInvalidMoves refactoring

--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -62,16 +62,20 @@ public class LogicTest {
     );
   }
 
-  @Test
-  public void testInvalidMove() {
-    Pair<Integer,Integer> knightPos = null;
+  private Pair<Integer,Integer> findKnightPosition() {
     for (int i = 0; i < 5; i++) {
       for (int j = 0; j < 5; j++) {
         if(this.logic.hasKnight(i, j)) {
-          knightPos = new Pair<>(i, j);
+          return new Pair<>(i, j);
         }
       }
     }
+    return null;
+  }
+
+  @Test
+  public void testInvalidMoves() {
+    Pair<Integer,Integer> knightPos = findKnightPosition();
 
     for (int i = 0; i < 5; i++) {
       for (int j = 0; j < 5; j++) {
@@ -79,19 +83,10 @@ public class LogicTest {
         int y = j - knightPos.getY();
         if (x==0 || y==0 || Math.abs(x)+Math.abs(y)!=3) {
           assertFalse(this.logic.hit(i, j));
-          Pair<Integer,Integer> newknightPos = null;
-
-          for (int i2 = 0; i2 < 5; i2++) {
-            for (int j2 = 0; j2 < 5; j2++) {
-              if(this.logic.hasKnight(i2, j2)) {
-                newknightPos = new Pair<>(i2, j2);
-              }
-            }
-          }
-          assertEquals(knightPos, newknightPos);
+          assertEquals(knightPos, findKnightPosition());
         }
       }
     }
-  }
 
+  }
 }
