116bff690ea77825c4a1dfc39f3602833abaf137
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2025-03-04 12:12:04 +0100

    testPawnInitialPosition testKinghtInitialPosition

--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -1,15 +1,35 @@
 package e2;
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LogicTest {
 
   @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
+  public void testKnightInitialPosition() {
+    Logics logic = new LogicsImpl(5);
+    int knightCounter = 0;
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+        if(logic.hasKnight(i, j)) {
+          knightCounter++;
+        }
+      }
+    }
+    assertEquals(1, knightCounter);
+  }
+
+  @Test
+  public void testPawnInitialPosition() {
+    Logics logic = new LogicsImpl(5);
+    int pawnCounter = 0;
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+        if(logic.hasPawn(i, j)) {
+          pawnCounter++;
+        }
+      }
+    }
+    assertEquals(1, pawnCounter);
   }
 }
